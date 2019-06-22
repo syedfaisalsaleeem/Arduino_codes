31a9cf95d8f38a1bc4ebe568c235eb8c855402d9
Syed Faisal Saleem <syedfaisalsaleem.100@gmail.com>
2019-06-22 17:21:55 +0500

    Add files via upload

---
+++ saturday_pid_control.py
@@ -0,0 +1,156 @@
+import serial
+import RPi.GPIO as IO            # calling header file for GPIO’s of PI
+import time
+from pandas import DataFrame
+
+previous_error=0
+integral=0
+setpoint=100
+KP=6
+KI=0
+KD=0.8 #0.2
+previous_error_off=0
+integral_off=0
+k_1=0
+KP_off=4.5
+KI_off=0
+KD_off=0
+Dt=40
+ser = serial.Serial('/dev/ttyACM0',9600)
+IO.setmode (IO.BOARD)       # programming the GPIO by BOARD pin numbers, GPIO21 is called as PIN40
+
+list_temperature=[None]
+list_time=[None]
+l_1=[]
+i=0
+m_1=0
+n=0
+#s = [0,1]
+def call_temp(temp):
+    return temp
+while True:
+        read_serial=ser.readline()
+        t=time.localtime()
+        current_time=time.strftime("%H%M%S",t)
+
+        x=l_1.append(str(read_serial))
+        
+        x=l_1[i][2:7]
+        i=i+1
+        print(x)
+        temp=float(x)
+
+        if temp!=None and temp <200:
+
+            list_time.append(current_time)
+            list_temperature.append(temp)
+            Value={'Time':list_time , 'Temperature':list_temperature}
+            df=DataFrame(Value,columns=['Time','Temperature'])
+            export_csv=df.to_csv(r'pid_100.csv',index=None,header=True)
+            presentvalue=temp
+            #print(presentvalue)
+            error=setpoint-presentvalue
+            integral=integral+error
+            derivative=(error-previous_error)
+            output=(KP*error)+(KI*integral)+(KD*derivative)
+            previous_error=error
+            output=int(output)
+            if(output>60 or output==60):
+                output=60
+                
+            print(output, derivative,integral,"output","derivative","integral")
+            #print(error,"error")
+            error_off=setpoint-presentvalue
+            integral_off=integral_off+error_off
+            derivative_off=(error_off-previous_error_off)
+            output_off=(KP_off*error_off)+(KI_off*integral_off)+(KD_off*derivative_off)
+            previous_error_off=error_off
+            output_off=int(output_off)
+            print(output_off,"output off")
+            if(output_off>60 or output_off==60 or output_off<0):
+                output_off=0
+                        
+            if (temp<70):
+               print('Heating')
+               
+               t=time.localtime()
+               current_time=time.strftime("%H%M%S",t)
+               read_serial_1=ser.readline()
+               y=l_1.append(str(read_serial_1))
+               y=l_1[i][2:7]
+               i=i+1
+               print(y)
+               #print(new_1)
+               m=float(y)
+               list_time.append(current_time)
+               list_temperature.append(m)
+               IO.setmode (IO.BOARD)
+               IO.setup(7,IO.OUT)
+               IO.output(7,1)
+            else:
+                
+                for new_1 in range(0,output):
+                   print('Heating')
+                   t=time.localtime()
+                   current_time=time.strftime("%H%M%S",t)
+                   read_serial_1=ser.readline()
+                   y=l_1.append(str(read_serial_1))
+                   y=l_1[i][2:7]
+                   i=i+1
+                   print(y)
+                   print(new_1)
+                   m=float(y)
+                   list_time.append(current_time)
+                   list_temperature.append(m)
+
+                   IO.setmode (IO.BOARD)
+                   IO.setup(7,IO.OUT)
+                   IO.output(7,1)
+                   
+                   if (m>setpoint):
+                      print(setpoint,'break')
+                      break 
+
+   
+                for t_1 in range(60,output_off,-1): 
+                        read_serial_1=ser.readline()
+                        t=time.localtime()
+                        current_time=time.strftime("%H%M%S",t)
+                        y=l_1.append(str(read_serial_1))
+                        y=l_1[i][2:7]
+                        i=i+1
+                        print(y)
+                        print(t_1)
+                        m=float(y)
+                        list_time.append(current_time)
+                        list_temperature.append(m)
+                        IO.setmode (IO.BOARD)
+                        IO.setup(7,IO.OUT)
+                        IO.output(7,0)
+                        if (m>setpoint):
+                          print(setpoint,'break')
+                          break 
+                #IO.cleanup()
+            #IO.setup(7,IO.OUT)
+            #IO.output(7,1)
+            
+            #time.sleep(error)
+            #IO.output(7,0)
+            #time.sleep(60-error)
+            #print(error)
+    
+        #while (m_1<1):
+        #IO.setup(7,IO.OUT)            # initialize digital pin40 as an output.
+        #IO.output(7,0)
+        #time.sleep(1)
+        #IO.output(7,0)
+        m_1=m_1+1
+            
+           # time.sleep(60)# turn the LED on (making the voltage level HIGH)
+##
+        #if(temp>100  and temp <110):
+            #break
+##    
+IO.cleanup() 
+        
+        
